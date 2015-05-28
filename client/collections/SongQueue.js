7f8f8c89cb01b50e5684b6abccdf8d6a0049bb72
Richard Treichel <rwtreichel@gmail.com>
2015-05-27 22:18:21 -0400

    Modify SongQueue's 'ended' event handler to play the next song in the queue after the current one ends.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,17 +2,26 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+
+    // When a new song is added to the queue, if it
+    // is the only song, play that song.
     this.on('add', function() {
       if (this.length === 1) {
         this.playFirst();
       }
     }, this);
 
+    // When a song in the queue ends, remove the song
+    // from the queue. If there are any songs left in
+    // the queue, play the next one.
     this.on('ended', function() {
       console.log("SongQueue's 'ended' event has fired.");
       var song = this.at(0);
-      console.log(song);
       this.remove(song);
+
+      if (this.length > 0) {
+        this.playFirst();
+      }
     }, this);
   },
 
