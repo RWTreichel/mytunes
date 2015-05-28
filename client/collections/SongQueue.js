c2184e9d296e16796f4cf93f36690c342924faa6
Richard Treichel <rwtreichel@gmail.com>
2015-05-27 20:22:02 -0400

    Register 'add' event handler for SongQueue instances upon creation, conditionally firing SongQueue.playFirst()

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,15 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+  },
 
 });
+
+SongQueue.prototype.playFirst = function() {
+  console.log("SongQueue.playFirst() has been called.");
+};
